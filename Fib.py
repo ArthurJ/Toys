04d970a17b20b98b631c90a01c76bd2eeb834182
Arthur Julião <azrael.zila@gmail.com>
2015-11-07 18:04:28 -0200

    Initial commit, códigos recuperados da minha biblioteca.

---
+++ Fib.py
@@ -0,0 +1,16 @@
+__inicial = {1: 1, 2: 1, 3: 2}
+
+
+def fib(n, descartar_anteriores=False, seq_fib=__inicial):
+    if n in seq_fib:
+        return seq_fib[n]
+    elif descartar_anteriores:
+        while n > max(seq_fib):
+            seq_fib[max(seq_fib) + 1] = \
+                seq_fib[(max(seq_fib))] + seq_fib[(max(seq_fib) - 1)]
+            del seq_fib[max(seq_fib) - 3]
+    else:
+        while n > max(seq_fib):
+            seq_fib[max(seq_fib) + 1] = \
+                seq_fib[(max(seq_fib))] + seq_fib[(max(seq_fib) - 1)]
+    return seq_fib[n]
