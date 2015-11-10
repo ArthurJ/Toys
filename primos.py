3019c4b9b71607f94c5129dd783481377da1b5f5
Arthur Julião <azrael.zila@gmail.com>
2015-11-10 17:16:36 -0200

    Melhoras no next_prime, para não armazenar números acima do necessário.

--- primos.py
+++ primos.py
@@ -4,6 +4,12 @@ import sys
 primes = [2, 3, 5, 7, 11, 13, 17, 19]
 
 
+class ListTooShortException(Exception):
+    def __init__(self, expr, msg):
+        self.expr = expr
+        self.msg = msg
+
+
 def is_prime(p, prime_list=primes):
     """
     :param p: Number to be tested
@@ -19,39 +25,40 @@ def is_prime(p, prime_list=primes):
         return True
 
     limite_de_teste = int(p**.5)
-    ultimo = 0
     for i in prime_list:
         if p % i == 0:
             return False
         if i > limite_de_teste:
             return True
-        ultimo = i
-    for i in range(ultimo, limite_de_teste, 2):
-        if p % i == 0:
-            return False
-        if i > limite_de_teste:
-            break
-    return True
+    raise ListTooShortException
 
 
 @time_it()
-def next_prime(n=1, prime_list=primes,  file=sys.stdout):
+def next_prime(n=1, prime_list=primes,  file=sys.stdout, cache_limit=False):
     """
     :param n: Number of next primes to generate, default=1
     :param prime_list: Initial list of primes
     :param file: Print primes in this file, default=sys.stout
+    :param cache_limit: Superior limit number to store in the prime_list
 
     Generate n primes, prints it, and stores it in the primes list
     """
     last = max(prime_list)
-    while True:
-        last += 2
-        if is_prime(last, prime_list=prime_list):
-            n -= 1
-            prime_list.append(last)
-            print(last, file=file)
-            if n <= 0:
-                break
+    try:
+        while True:
+            last += 2
+            if is_prime(last, prime_list=prime_list):
+                n -= 1
+                print(last, file=file)
+                if not cache_limit or (cache_limit and last < cache_limit):
+                    prime_list.append(last)
+                if n <= 0:
+                    break
+    except ListTooShortException:
+        print('Too limited cache to continue!', file=sys.stderr)
+
+
+next_prime(14488, prime_list=[2, 3])
 
 
 @time_it()
@@ -65,6 +72,7 @@ def crivo(lim_superior, file=sys.stdout):
     Essa implementação é mais lenta que o next_prime() para encontrar a
         mesma quantidade de números primos.
     """
+    # TODO reimplementar
     candidatos = range(1, lim_superior)
     primialidade = [True]*lim_superior
     primialidade[0] = False
