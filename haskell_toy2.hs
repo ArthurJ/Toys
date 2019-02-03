bf697b22b49f66dacf2ac65299e00f0a1d26291a
Arthur Julião <azrael.zila@gmail.com>
2019-02-02 22:22:35 -0200

    Ex. Hr

--- haskell_toy2.hs
+++ haskell_toy2.hs
@@ -8,3 +8,8 @@ somaChar = sum . map digitToInt . show
 
 catal :: Map.Map String String
 catal = Map.fromList $ [("a", "1")]
+
+--e :: Integer -> Float ->  Float
+e :: (Floating a) => Integer -> a ->  a
+e 0 _ = 1
+e n x = ((x^n)/fromIntegral (product [1..n] )) + e (n-1) x
