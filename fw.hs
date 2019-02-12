6a4747485efdbfe619a35232e2a3f245a8ebfb1b
Arthur Julião <azrael.zila@gmail.com>
2019-02-12 01:08:43 -0200

    Floyd-Warshall, to be tested

--- fw.hs
+++ fw.hs
@@ -6,7 +6,7 @@ chunksOf n lst = Split.chunksOf n lst
 
 
 infinities :: Int -> [[Int]]
-infinities n = take n (repeat (take n (repeat (maxBound :: Int))))  
+infinities n = take n (repeat (take n (repeat 999)))  
 
 
 initial 0 lst = []
@@ -14,15 +14,17 @@ initial n lst = (head lst):(initial (n-1) (tail lst))
 
 
 chgLine :: Int -> Int -> [Int] -> [Int]
+chgLine pos _ line 
+    | pos >= length line = line
 chgLine pos val line = (initial pos line) ++ val : (drop (pos+1) line)
 
 
 zeroDiagInternal :: Int -> [[Int]] -> [[Int]]
-zeroDiagInternal 0 matrix = matrix
+zeroDiagInternal (-1) matrix = matrix
 zeroDiagInternal pos matrix = chgLine pos 0 (last matrix):(zeroDiagInternal (pos-1) (init matrix))
 
 zeroDiag :: [[Int]] -> [[Int]]
-zeroDiag matrix = reverse (zeroDiagInternal (length matrix) matrix)
+zeroDiag matrix = reverse (zeroDiagInternal ((length matrix)-1) matrix)
 
 
 chgMatrixInternal :: Int -> Int -> Int -> [[Int]] -> [[Int]]
@@ -36,6 +38,17 @@ chgMatrix i j _ lst
     where len = length lst
 chgMatrix i j val matrix = chgMatrixInternal i j val matrix
 
+calcMinimumInternal :: Int -> Int -> Int -> [[Int]] -> [[Int]]
+calcMinimumInternal i j k matrix 
+    | i == len  = matrix
+    | k == len  = calcMinimumInternal i (j+1) 0 matrix 
+    | i == j    = calcMinimumInternal i (j+1) k matrix
+    | j == len  = calcMinimumInternal (i+1) 0 0 matrix
+    | otherwise = calcMinimumInternal i j (k+1) (chgMatrix i j val matrix)    
+    where   val = min (matrix !! i !! j) ((matrix !! i !! k)+(matrix !! k !! j))
+            len = (length matrix)
+    -- | k == j    = calcMinimumInternal i j (k+1) matrix
+    -- | k == i    = calcMinimumInternal i j (k+1) matrix
 
--- getMin matrix i j k = min (matrix !! i !! j) ((matrix !! i !! k)+(matrix !! k !! j))
--- setMinInternal matrix i j k = chgMatrix i j (getMin matrix i j k)
+
+floydWarshall matrix = calcMinimumInternal 0 0 0 matrix
