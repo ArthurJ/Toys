4c8cc19aee77195628423b9dc399104667e56206
Arthur Julião <ArthurJ@users.noreply.github.com>
2020-12-09 13:35:31 -0300

    Update prime.hs
    Removendo o teste extra no crivo de Erastostenes. Em todos os testes de desempenho essa versão mais simples se saiu significativamente melhor.

--- prime.hs
+++ prime.hs
@@ -19,12 +19,9 @@ next_n_primes n lst | n <= 0 = lst
                     | otherwise = next_n_primes (n-1) (next_prime 0 lst) 
 
 -- Mais conciso e direto, porém mais lento.
-primos = filtraPrimos [2..]
-  where filtraPrimos (p:xs) =
-          p : filtraPrimos [x | x <- xs , 
-                            let s=(ceiling (sqrt (fromInteger x))),
-                            p>s || 
-                            x `mod` p /= 0
-                          ]
+crivo :: [Integer] -> [Integer]
+crivo (p:ps) = p : crivo [x | x <- ps, mod x p /= 0 ]
+
+primos = crivo [2..]
 
 main =  print $ next_n_primes 5000 prime_list
